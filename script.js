4987e7328a2c185ecbb6f9130b3a7e669afda612
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-02-14 08:03:47 +0100

    Added headline

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
+/**
+ * Array to store fetched Pokémon details.
+ * @type {Array}
+ */
 let pokemonDetails = [];
+
+/**
+ * Base URL for the Pokémon API.
+ * @constant {string}
+ */
 const BASE_URL = "https://pokeapi.co/api/v2/";
+
+/**
+ * Offset for paginated API requests.
+ * @type {number}
+ */
 let offset = 0;
+
+/**
+ * Number of Pokémon to fetch per request.
+ * @constant {number}
+ */
 const limit = 30;
 
-const typeColor = { bug: "#26de81", dragon: "#ffeaa7", electric: "#fed330", fairy: "#FF0069", 
-  fighting: "#30336b", fire: "#f0932b", flying: "#81ecec", grass: "#00b894", 
-  ground: "#EFB549", ghost: "#a55eea", ice: "#74b9ff", normal: "#95afc0", 
-  poison: "#6c5ce7", psychic: "#a29bfe", rock: "#2d3436", water: "#0190FF" };
+/**
+ * Mapping of Pokémon types to their respective colors.
+ * @constant {Object}
+ */
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
 
-const loadingIndicator = document.getElementById('loading');
-const loadMoreButton = document.getElementById('load-more');
+/**
+ * Loading indicator element.
+ * @constant {HTMLElement}
+ */
+const loadingIndicator = document.getElementById("loading");
 
+/**
+ * Load more button element.
+ * @constant {HTMLElement}
+ */
+const loadMoreButton = document.getElementById("load-more");
+
+/**
+ * Initializes the application by fetching Pokémon data and setting up search functionality.
+ * @async
+ */
 async function init() {
   showLoading();
   await fetchPokemonData();
@@ -18,12 +68,19 @@ async function init() {
   hideLoading();
 }
 
+/**
+ * Fetches Pokémon data and updates the UI.
+ * @async
+ */
 async function fetchPokemonData() {
   const pokemonCache = {};
   try {
     await manageFetchLoading();
     const data = await fetchPokemons();
-    const newPokemonDetails = await fetchPokemonDetails(data.results, pokemonCache);
+    const newPokemonDetails = await fetchPokemonDetails(
+      data.results,
+      pokemonCache
+    );
     pokemonDetails.push(...newPokemonDetails);
     offset += limit;
     renderPokemon();
@@ -34,118 +91,221 @@ async function fetchPokemonData() {
   }
 }
 
+/**
+ * Manages loading state during data fetching.
+ * @async
+ */
 async function manageFetchLoading() {
   showLoading();
   loadMoreButton.disabled = true;
 }
 
+/**
+ * Fetches a list of Pokémon from the API.
+ * @async
+ * @returns {Promise<Object>} The response data containing Pokémon list.
+ */
 async function fetchPokemons() {
-  const response = await fetch(`${BASE_URL}pokemon?offset=${offset}&limit=${limit}`);
+  const response = await fetch(
+    `${BASE_URL}pokemon?offset=${offset}&limit=${limit}`
+  );
   if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
   return await response.json();
 }
 
+/**
+ * Fetches detailed data for each Pokémon.
+ * @async
+ * @param {Array} results - List of Pokémon from the API.
+ * @param {Object} cache - Cache object to store already fetched details.
+ * @returns {Promise<Array>} Array of Pokémon details.
+ */
 async function fetchPokemonDetails(results, cache) {
-  return await Promise.all(results.map(async (pokemon) => {
-    if (cache[pokemon.name]) return cache[pokemon.name];
-    const details = await fetchPokemonDetail(pokemon.url);
-    cache[pokemon.name] = details;
-    return details;
-  }));
+  return await Promise.all(
+    results.map(async (pokemon) => {
+      if (cache[pokemon.name]) return cache[pokemon.name];
+      const details = await fetchPokemonDetail(pokemon.url);
+      cache[pokemon.name] = details;
+      return details;
+    })
+  );
 }
 
+/**
+ * Fetches detailed information for a single Pokémon.
+ * @async
+ * @param {string} url - API URL for the Pokémon details.
+ * @returns {Promise<Object>} The Pokémon detail data.
+ */
 async function fetchPokemonDetail(url) {
   const res = await fetch(url);
   return await res.json();
 }
 
+/**
+ * Handles errors occurring during fetch operations.
+ * @param {Error} error - The error object.
+ */
 function handleFetchError(error) {
   console.error("Fetch error:", error);
   showError("Failed to load Pokémon data. Please try again later.");
 }
 
+/**
+ * Resets the UI state after data fetching.
+ */
 function resetFetchLoading() {
   hideLoading();
   loadMoreButton.disabled = false;
 }
 
+/**
+ * Initializes search functionality for filtering Pokémon.
+ */
 function initSearch() {
   const searchInput = document.getElementById("search-input");
   let timeoutId;
 
   searchInput.addEventListener("input", (e) => {
     const searchTerm = e.target.value.toLowerCase();
-    if (searchTerm.length < 3) return renderPokemon(pokemonDetails.slice(0, 30));
+    if (searchTerm.length < 3)
+      return renderPokemon(pokemonDetails.slice(0, 30));
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => handleSearch(searchTerm), 300);
   });
 }
 
+/**
+ * Handles search input to filter Pokémon based on user input.
+ * @param {string} searchTerm - The user's search term.
+ */
 function handleSearch(searchTerm) {
-  const filtered = pokemonDetails.filter(pokemon => pokemon.name.toLowerCase().startsWith(searchTerm)).slice(0, 10);
-  renderPokemon(filtered.length ? filtered : displayError(`Keine Pokémon gefunden für "${searchTerm}".`));
+  const filtered = pokemonDetails
+    .filter((pokemon) => pokemon.name.toLowerCase().startsWith(searchTerm))
+    .slice(0, 10);
+  renderPokemon(
+    filtered.length
+      ? filtered
+      : displayError(`Keine Pokémon gefunden für "${searchTerm}".`)
+  );
 }
 
+/**
+ * Renders Pokémon data in the UI.
+ * @param {Array} [pokemonArray=pokemonDetails] - Array of Pokémon to render.
+ */
 function renderPokemon(pokemonArray = pokemonDetails) {
   const pokedexContainer = document.getElementById("pokedex-container");
   const fragment = document.createDocumentFragment();
-  
-  pokemonArray.forEach(pokemon => fragment.appendChild(createPokemonCard(pokemon)));  
+
+  pokemonArray.forEach((pokemon) =>
+    fragment.appendChild(createPokemonCard(pokemon))
+  );
   pokedexContainer.replaceChildren(fragment);
   addCardHoverEffects();
 }
 
+/**
+ * Creates a Pokémon card element.
+ * @param {Object} pokemon - The Pokémon data.
+ * @returns {HTMLElement} The Pokémon card element.
+ */
 function createPokemonCard(pokemon) {
   const card = document.createElement("div");
   card.className = "pokemon-card";
-  card.dataset.name = pokemon.name; 
-  card.style.backgroundColor = typeColor[pokemon.types[0].type.name] || "#ffffff";
+  card.dataset.name = pokemon.name;
+  card.style.backgroundColor =
+    typeColor[pokemon.types[0].type.name] || "#ffffff";
   setInnerHTML(card, pokemon);
   return card;
 }
 
+/**
+ * Sets the inner HTML of a Pokémon card.
+ * @param {HTMLElement} card - The card element.
+ * @param {Object} pokemon - The Pokémon data.
+ */
 function setInnerHTML(card, pokemon) {
   card.innerHTML = createPokemonCardTemplate(pokemon);
 }
 
+/**
+ * Creates a type button element.
+ * @param {Object} type - The Pokémon type.
+ * @returns {string} The HTML string for the type button.
+ */
 function createTypeButton(type) {
   return `
     <button class="type-button">
-      <img class="type-icon" src="${getTypeIconSrc(type.type.name)}" alt="${type.type.name}">
+      <img class="type-icon" src="${getTypeIconSrc(type.type.name)}" alt="${
+    type.type.name
+  }">
       <span>${type.type.name}</span>
     </button>
   `;
 }
 
+/**
+ * Displays an error message in the UI.
+ * @param {string} message - The error message.
+ */
 function displayError(message) {
   const container = document.getElementById("pokedex-container");
   container.innerHTML = `<div class="error-message"><p>${message}</p><button onclick="init()">Wiederholen</button></div>`;
 }
 
+/**
+ * Retrieves the icon source for a given Pokémon type.
+ * @param {string} type - The Pokémon type.
+ * @returns {string} The file path of the corresponding type icon.
+ */
 function getTypeIconSrc(type) {
-  const typeIcons = { water: "imgs/icons/water.png", grass: "imgs/icons/grass.png", 
-    fire: "imgs/icons/fire.png", normal: "imgs/icons/normal.png", bug: "imgs/icons/bug.png", 
-    poison: "imgs/icons/poison.png", electric: "imgs/icons/electric.png", ground: "imgs/icons/ground.png", 
-    flying: "imgs/icons/flying.png", psychic: "imgs/icons/psychic.png", 
-    fairy: "imgs/icons/fairy.png", fighting: "imgs/icons/fighting.png", 
-    rock: "imgs/icons/rock.png", steel: "imgs/icons/steel.png", ice: "imgs/icons/ice.png", 
-    ghost: "imgs/icons/ghost.png", dark: "imgs/icons/dark.png", dragon: "imgs/icons/dragon.png" 
+  const typeIcons = {
+    water: "imgs/icons/water.png",
+    grass: "imgs/icons/grass.png",
+    fire: "imgs/icons/fire.png",
+    normal: "imgs/icons/normal.png",
+    bug: "imgs/icons/bug.png",
+    poison: "imgs/icons/poison.png",
+    electric: "imgs/icons/electric.png",
+    ground: "imgs/icons/ground.png",
+    flying: "imgs/icons/flying.png",
+    psychic: "imgs/icons/psychic.png",
+    fairy: "imgs/icons/fairy.png",
+    fighting: "imgs/icons/fighting.png",
+    rock: "imgs/icons/rock.png",
+    steel: "imgs/icons/steel.png",
+    ice: "imgs/icons/ice.png",
+    ghost: "imgs/icons/ghost.png",
+    dark: "imgs/icons/dark.png",
+    dragon: "imgs/icons/dragon.png",
   };
   return typeIcons[type] || "imgs/icons/default.png";
 }
 
+/**
+ * Adds hover effects to Pokémon cards.
+ */
 function addCardHoverEffects() {
   const cards = document.querySelectorAll(".pokemon-card");
-  cards.forEach(card => {
+  cards.forEach((card) => {
     card.addEventListener("mousemove", handleHover);
     card.addEventListener("mouseleave", resetCard);
   });
 }
 
+/**
+ * Handles mouse hover event on Pokémon cards.
+ * @param {MouseEvent} e - The mouse event.
+ */
 function handleHover(e) {
   setHoverEffect.call(this, e);
 }
 
+/**
+ * Sets hover effect based on mouse position.
+ * @param {MouseEvent} e - The mouse event.
+ */
 function setHoverEffect(e) {
   const { clientX, clientY } = e;
   const { left, top, width, height } = this.getBoundingClientRect();
@@ -155,6 +315,9 @@ function setHoverEffect(e) {
   this.style.setProperty("--y", `${y * 100}%`);
 }
 
+/**
+ * Resets the hover effect on Pokémon cards.
+ */
 function resetCard() {
   this.style.setProperty("--x", "50%");
   this.style.setProperty("--y", "50%");
@@ -162,27 +325,50 @@ function resetCard() {
 
 document.addEventListener("DOMContentLoaded", init);
 
+/**
+ * Creates the details HTML for a Pokémon.
+ * @param {Object} pokemon - The Pokémon data.
+ * @returns {string} The HTML string for the details section.
+ */
 function createDetailsHTML(pokemon) {
   const height = (pokemon.height / 10).toFixed(1);
   const weight = (pokemon.weight / 10).toFixed(1);
-  const abilities = pokemon.abilities.map(a => a.ability.name).join(", ");
-  return detailTemplate(pokemon, height, weight, abilities) 
+  const abilities = pokemon.abilities.map((a) => a.ability.name).join(", ");
+  return detailTemplate(pokemon, height, weight, abilities);
 }
 
+/**
+ * Creates a row for a Pokémon's stats.
+ * @param {Object} stat - The Pokémon stat data.
+ * @returns {string} The HTML string for the stat row.
+ */
 function createStatRow(stat) {
   return statRowTemplate(stat);
 }
 
+/**
+ * Capitalizes the first letter of a string.
+ * @param {string} string - The input string.
+ * @returns {string} The formatted string.
+ */
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/**
+ * Handles tab switching functionality.
+ * @param {Event} evt - The event triggering the tab switch.
+ * @param {string} tabName - The name of the tab to open.
+ */
 function openTab(evt, tabName) {
   hideAllTabs();
   showActiveTab(tabName);
   evt.currentTarget.classList.add("active");
 }
 
+/**
+ * Hides all tab contents and removes active state from tab buttons.
+ */
 function hideAllTabs() {
   const tabContent = document.getElementsByClassName("tab-content");
   for (let i = 0; i < tabContent.length; i++) {
@@ -193,25 +379,37 @@ function hideAllTabs() {
     tabButtons[i].classList.remove("active");
   }
 }
-
+/**
+ * Displays the specified tab content.
+ * @param {string} tabName - The name of the tab to show.
+ */
 function showActiveTab(tabName) {
   document.getElementById(tabName).style.display = "block";
 }
 
+/**
+ * Shows the loading indicator.
+ */
 function showLoading() {
-  loadingIndicator.removeAttribute('hidden');
+  loadingIndicator.removeAttribute("hidden");
 }
 
+/**
+ * Hides the loading indicator.
+ */
 function hideLoading() {
-  loadingIndicator.setAttribute('hidden', 'true');
+  loadingIndicator.setAttribute("hidden", "true");
 }
 
+/**
+ * Displays the details of a selected Pokémon in an overlay.
+ * @param {Object} pokemon - The Pokémon data.
+ */
 function showPokemonDetails(pokemon) {
   const overlay = createOverlay();
-  const detailsCard = document.createElement("div");
+  const detailsCard = document.createElement("div");  
   detailsCard.className = "details-card";
   detailsCard.innerHTML = createDetailsHTML(pokemon);
-  
   appendNavigationButtons(detailsCard, pokemon);
   overlay.appendChild(detailsCard);
   document.body.appendChild(overlay);
@@ -222,63 +420,102 @@ function showPokemonDetails(pokemon) {
   });
 }
 
+/**
+ * Creates an overlay element for displaying Pokémon details.
+ * @returns {HTMLElement} The overlay element.
+ */
 function createOverlay() {
   const overlay = document.createElement("div");
   overlay.className = "overlay";
   return overlay;
 }
 
+/**
+ * Appends navigation buttons to a Pokémon details card.
+ * @param {HTMLElement} detailsCard - The details card element.
+ * @param {Object} currentPokemon - The current Pokémon data.
+ */
 function appendNavigationButtons(detailsCard, currentPokemon) {
-  detailsCard.appendChild(createButton('prev', currentPokemon));
-  detailsCard.appendChild(createButton('next', currentPokemon));
+  const arrowDiv = document.createElement("div");
+  arrowDiv.id = "arrowDiv";
+  arrowDiv.appendChild(createButton("prev", currentPokemon));
+  arrowDiv.appendChild(createButton("next", currentPokemon));
+  detailsCard.appendChild(arrowDiv);
 }
 
+/**
+ * Creates a navigation button for switching Pokémon details.
+ * @param {string} direction - The navigation direction ('prev' or 'next').
+ * @param {Object} currentPokemon - The current Pokémon data.
+ * @returns {HTMLElement} The navigation button element.
+ */
 function createButton(direction, currentPokemon) {
   const button = document.createElement("button");
-  button.textContent = direction === 'prev' ? '<' : '>';
+  button.textContent = direction === "prev" ? "<" : ">";
   button.className = `arrow-button ${direction}`;
   button.addEventListener("click", (e) => {
     e.stopPropagation();
-    direction === 'prev' ? showPreviousPokemon(currentPokemon) : showNextPokemon(currentPokemon);
+    direction === "prev"
+      ? showPreviousPokemon(currentPokemon)
+      : showNextPokemon(currentPokemon);
   });
   return button;
 }
 
+/**
+ * Displays the previous Pokémon in the details overlay.
+ * @param {Object} currentPokemon - The currently displayed Pokémon.
+ */
 function showPreviousPokemon(currentPokemon) {
   const currentIndex = pokemonDetails.indexOf(currentPokemon);
-  const prevIndex = (currentIndex - 1 + pokemonDetails.length) % pokemonDetails.length;
+  const prevIndex =
+    (currentIndex - 1 + pokemonDetails.length) % pokemonDetails.length;
   updateDetailsCard(pokemonDetails[prevIndex]);
 }
 
+/**
+ * Displays the next Pokémon in the details overlay.
+ * @param {Object} currentPokemon - The currently displayed Pokémon.
+ */
 function showNextPokemon(currentPokemon) {
   const currentIndex = pokemonDetails.indexOf(currentPokemon);
   const nextIndex = (currentIndex + 1) % pokemonDetails.length;
   updateDetailsCard(pokemonDetails[nextIndex]);
 }
 
+/**
+ * Updates the Pokémon details card with new data.
+ * @param {Object} pokemon - The Pokémon data.
+ */
 function updateDetailsCard(pokemon) {
   const detailsCard = document.querySelector(".details-card");
   if (detailsCard) {
-    detailsCard.style.opacity = 0; 
+    detailsCard.style.opacity = 0;
 
     setTimeout(() => {
-      detailsCard.innerHTML = createDetailsHTML(pokemon); 
+      detailsCard.innerHTML = createDetailsHTML(pokemon);
       appendNavigationButtons(detailsCard, pokemon);
-      detailsCard.style.opacity = 1; 
-    }, 300); 
+      detailsCard.style.opacity = 1;
+    }, 300);
   }
 }
 
+/**
+ * Closes the Pokémon details overlay.
+ * @param {HTMLElement} overlay - The overlay element to be removed.
+ */
 function closeOverlay(overlay) {
   document.body.removeChild(overlay);
   document.body.classList.remove("no-scroll");
 }
 
-document.getElementById('load-more').addEventListener('click', fetchPokemonData);
+document
+  .getElementById("load-more")
+  .addEventListener("click", fetchPokemonData);
 document.getElementById("pokedex-container").addEventListener("click", (e) => {
-  if (e.target.closest('.pokemon-card')) {
-    const pokemonName = e.target.closest('.pokemon-card').dataset.name;
-    const pokemon = pokemonDetails.find(p => p.name === pokemonName);
+  if (e.target.closest(".pokemon-card")) {
+    const pokemonName = e.target.closest(".pokemon-card").dataset.name;
+    const pokemon = pokemonDetails.find((p) => p.name === pokemonName);
     showPokemonDetails(pokemon);
   }
 });
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,27 +1,45 @@
+/**
+ * Generates an HTML template for a Pokémon card.
+ * @param {Object} pokemon - The Pokémon data.
+ * @returns {string} The HTML template for the Pokémon card.
+ */
 function createPokemonCardTemplate(pokemon) {
-    const typesButtons = pokemon.types.map(type => createTypeButton(type)).join("");
-  
-    return `
-      <div class="pokemon-card-header">
-        <h3 class="pokemon-name">${pokemon.name}</h3>
-        <p class="pokemon-number">${pokemon.id.toString().padStart(3, "0")}</p>
-      </div>
-      <div class="pokemon-image-container">
-        <img class="pokemon-image" src="${pokemon.sprites.other["official-artwork"].front_default || pokemon.sprites.front_default}" alt="Offizielle Abbildung von ${pokemon.name}">
-      </div>
-      <div class="pokemon-card-footer">
-        ${typesButtons}
-      </div>
-    `;
-  }
+  const typesButtons = pokemon.types.map(type => createTypeButton(type)).join("");
 
-  function detailTemplate (pokemon, height, weight, abilities) {
-    return `
+  return `
+    <div class="pokemon-card-header">
+      <h3 class="pokemon-name">${pokemon.name}</h3>
+      <p class="pokemon-number">${pokemon.id.toString().padStart(3, "0")}</p>
+    </div>
+    <div class="pokemon-image-container">
+      <img class="pokemon-image" src="${pokemon.sprites.other["official-artwork"].front_default || pokemon.sprites.front_default}" alt="Official artwork of ${pokemon.name}">
+    </div>
+    <div class="pokemon-card-footer">
+      ${typesButtons}
+    </div>
+  `;
+}
+
+/**
+* Generates an HTML template for the Pokémon details overlay.
+* @param {Object} pokemon - The Pokémon data.
+* @param {number} height - The height of the Pokémon in meters.
+* @param {number} weight - The weight of the Pokémon in kilograms.
+* @param {string} abilities - The abilities of the Pokémon.
+* @returns {string} The HTML template for the Pokémon details.
+*/
+function detailTemplate (pokemon, height, weight, abilities) {
+  const typeButtons = pokemon.types.map(type => createTypeButton(type)).join("");
+
+  return `
     <div class="details-header">
       <h2 style="display: inline-block; margin-right: 10px; text-transform: capitalize;">${pokemon.name}</h2>
       <span style="display: inline-block;">${pokemon.id.toString().padStart(3, "0")}</span>
     </div>
     <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" class="details-image">
+    <div class="type-button-container">
+      ${typeButtons}
+    </div>
     <div class="detail-overlay">
       <div class="tab-container">
         <button class="tab-button active" onclick="openTab(event, 'About')">About</button>
@@ -40,16 +58,21 @@ function createPokemonCardTemplate(pokemon) {
       <div id="BaseStats" class="tab-content" style="display: none;">
         ${pokemon.stats.map(stat => createStatRow(stat)).join("")}
       </div>
-    </div>
+    </div>     
   `;
-  }
+}
 
-  function statRowTemplate(stat){
-    return`
-    <div class="stat-row">
-      <span>${capitalizeFirstLetter(stat.stat.name)}</span>
-      <progress value="${stat.base_stat}" max="200"></progress>
-      <span>${stat.base_stat}</span>
-    </div>
-  `;
+/**
+* Generates an HTML row template for a Pokémon's stats.
+* @param {Object} stat - The stat object containing name and base value.
+* @returns {string} The HTML template for a stat row.
+*/
+function statRowTemplate(stat){
+  return`
+  <div class="stat-row">
+    <span>${capitalizeFirstLetter(stat.stat.name)}</span>
+    <progress value="${stat.base_stat}" max="200"></progress>
+    <span>${stat.base_stat}</span>
+  </div>
+`;
 }
